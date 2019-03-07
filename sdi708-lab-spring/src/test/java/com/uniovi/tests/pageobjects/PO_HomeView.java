fc389ca65697a7d216d2da66196904f7cdb1f489
pacojq <pacojuanquiros@gmail.com>
2019-03-07 01:37:52 +0100

    SDI-Selenium-5.4-CasosUso-VistaHome

---
+++ sdi708-lab-spring/src/test/java/com/uniovi/tests/pageobjects/PO_HomeView.java
@@ -0,0 +1,83 @@
+package com.uniovi.tests.pageobjects;
+
+import static org.junit.Assert.assertTrue;
+import java.util.List;
+import org.openqa.selenium.*;
+import com.uniovi.tests.util.SeleniumUtils;
+
+public class PO_HomeView extends PO_View {
+
+	
+	/**
+	 * CLicka una de las opciones principales (a href) y comprueba que se vaya a la
+	 * vista con el elemento de tipo type con el texto Destino
+	 * 
+	 * @param driver: apuntando al navegador abierto actualmente.
+	 * @param textOption: Texto de la opción principal.
+	 * @param criterio: "id" or "class" or "text" or "@attribute" or "free". Si el
+	 *        valor de criterio es free es una expresion xpath completa.
+	 * @param textoDestino: texto correspondiente a la búsqueda de la página
+	 *        destino.
+	 */
+	public static void clickOption(WebDriver driver, String textOption, String criterio, String textoDestino) {
+		// CLickamos en la opción de registro y esperamos a que se cargue el enlace de Registro. 
+		List<WebElement> elementos = SeleniumUtils.EsperaCargaPagina(driver, "@href", textOption,
+				getTimeout());
+		// Tiene que haber un sólo elemento.
+		assertTrue(elementos.size() == 1);
+		// Ahora lo clickamos
+		elementos.get(0).click();
+		// Esperamos a que sea visible un elemento concreto
+		elementos = SeleniumUtils.EsperaCargaPagina(driver, criterio, textoDestino, getTimeout());
+		// Tiene que haber un sólo elemento.
+		assertTrue(elementos.size() == 1);
+	}
+	
+	
+	
+	/**
+	* Selecciona el enlace de idioma correspondiente al texto textLanguage
+	* @param driver: apuntando al navegador abierto actualmente.
+	* @param textLanguage: el texto que aparece en el enlace de idioma ("English" o
+	"Spanish")
+	*/
+	public static void changeIdiom(WebDriver driver, String textLanguage) {
+		//clickamos la opción Idioma.
+		List<WebElement> elementos = SeleniumUtils.EsperaCargaPagina(driver, "id",
+		"btnLanguage", getTimeout());
+		elementos.get(0).click();
+		//Esperamos a que aparezca el menú de opciones.
+		elementos = SeleniumUtils.EsperaCargaPagina(driver, "id",
+		"languageDropdownMenuButton", getTimeout());
+		//SeleniumUtils.esperarSegundos(driver, 2);
+		//CLickamos la opción Inglés partiendo de la opción Español
+		elementos = SeleniumUtils.EsperaCargaPagina(driver, "id", textLanguage,
+		getTimeout());
+		elementos.get(0).click();
+	}
+	
+	
+	
+	static public void checkWelcome(WebDriver driver, int language) {
+		//Esperamos a que se cargue el saludo de bienvenida en Español
+		SeleniumUtils.EsperaCargaPagina(driver, "text", p.getString("welcome.message", 
+				language), getTimeout());
+	}
+
+	
+	static public void checkChangeIdiom(WebDriver driver, String textIdiom1, String textIdiom2, int locale1,
+			int locale2) {
+		// Esperamos a que se cargue el saludo de bienvenida en Español
+		PO_HomeView.checkWelcome(driver, locale1);
+		// Cambiamos a segundo idioma
+		PO_HomeView.changeIdiom(driver, textIdiom2);
+		// COmprobamos que el texto de bienvenida haya cambiado a segundo idioma
+		PO_HomeView.checkWelcome(driver, locale2);
+		// Volvemos a Español.
+		PO_HomeView.changeIdiom(driver, textIdiom1);
+		// Esperamos a que se cargue el saludo de bienvenida en Español
+		PO_HomeView.checkWelcome(driver, locale1);
+	}
+	
+	
+}
