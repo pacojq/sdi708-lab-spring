9c6aa4a0b2fff6ca47e8735d610cc25cf23e76cd
pacojq <pacojuanquiros@gmail.com>
2019-03-07 01:47:59 +0100

    SDI-Selenium-5.6-CasosUso-VistaLogin

---
+++ sdi708-lab-spring/src/test/java/com/uniovi/tests/pageobjects/PO_LoginView.java
@@ -0,0 +1,25 @@
+package com.uniovi.tests.pageobjects;
+
+import static org.junit.Assert.assertTrue;
+import java.util.List;
+import org.openqa.selenium.*;
+import com.uniovi.tests.util.SeleniumUtils;
+
+public class PO_LoginView extends PO_NavView {
+
+	
+	static public void fillForm(WebDriver driver, String dnip, String passwordp) {
+		
+		WebElement dni = driver.findElement(By.name("dni"));
+		dni.click();
+		dni.clear();
+		dni.sendKeys(dnip);
+		WebElement password = driver.findElement(By.name("password"));
+		password.click();
+		password.clear();
+		password.sendKeys(passwordp);
+		By boton = By.className("btn");
+		driver.findElement(boton).click();
+	}
+
+}
